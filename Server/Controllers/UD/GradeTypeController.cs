f2d773741f9f877d0a8abd75144932d4ad19ce1a
Sean Johnson <seanjohn@udel.edu>
2023-11-13 00:38:16 -0500

    Did grade type controller and dto skeleton

---
+++ Server/Controllers/UD/GradeTypeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OCTOBER.EF.Data;
+using OCTOBER.EF.Models;
+using OCTOBER.Shared;
+using Telerik.DataSource;
+using Telerik.DataSource.Extensions;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System.Linq.Dynamic.Core;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis;
+using AutoMapper;
+using OCTOBER.Server.Controllers.Base;
+using OCTOBER.Shared.DTO;
+
+namespace OCTOBER.Server.Controllers.UD
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GradeTypeController : BaseController, GenericRestController<GradeTypeDTO>
+    {
+        public GradeTypeController(OCTOBEROracleContext context,
+            IHttpContextAccessor httpContextAccessor,
+            IMemoryCache memoryCache)
+        : base(context, httpContextAccessor)
+        {
+        }
+
+        public Task<IActionResult> Delete(int KeyVal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Get()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IActionResult> Get(int KeyVal)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("Get/{SchoolID}/{GradeTypeCode}")]
+        public async Task<IActionResult> Get(int SchoolID, string GradeTypeCode)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                GradeTypeDTO? result = await _context
+                    .GradeTypes
+                    .Where(x => x.SchoolId == SchoolID)
+                    .Where(x => x.GradeTypeCode == GradeTypeCode)
+                     .Select(sp => new GradeTypeDTO
+                     {
+                          CreatedBy = sp.CreatedBy,
+                           CreatedDate = sp.CreatedDate,
+                            Description = sp.Description,
+                             GradeTypeCode = sp.GradeTypeCode,
+                              ModifiedBy = sp.ModifiedBy,
+                               ModifiedDate = sp.ModifiedDate,
+                                SchoolId = SchoolID
+                                
+                     })
+                .SingleOrDefaultAsync();
+
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        public Task<IActionResult> Post([FromBody] GradeTypeDTO _T)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Put([FromBody] GradeTypeDTO _T)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
